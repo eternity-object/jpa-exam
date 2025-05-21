1188534a125381ba980633362073fa50a16b3945
seungyeonhur <seungyeonhur@hyundai-autoever.com>
2025-05-21 16:40:30 +0900

    허승연 과제 제출

--- src/main/java/org/eternity/domainmodel/movie/domain/DiscountCondition.java
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountCondition.java
@@ -1,13 +1,28 @@
 package org.eternity.domainmodel.movie.domain;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 
 @Entity
 @Getter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "CONDITION_TYPE")
 public abstract class DiscountCondition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "condition_sequence")
     private Long id;
 
+    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private DiscountPolicy policy;
 
     abstract public void load();
--- src/main/java/org/eternity/domainmodel/movie/domain/DiscountPolicy.java
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountPolicy.java
@@ -1,8 +1,20 @@
 package org.eternity.domainmodel.movie.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.NamedAttributeNode;
+import jakarta.persistence.NamedEntityGraph;
+import jakarta.persistence.NamedEntityGraphs;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -10,8 +22,13 @@ import java.util.Set;
 @Entity
 @NoArgsConstructor @Getter
 public class DiscountPolicy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "POLICY_ID")
+    @Fetch(FetchMode.SUBSELECT)
     private Set<DiscountCondition> conditions = new HashSet<>();
 
     public DiscountPolicy(DiscountCondition ... conditions) {
