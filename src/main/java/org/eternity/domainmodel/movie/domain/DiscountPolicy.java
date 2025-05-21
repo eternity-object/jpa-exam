66f5f778c979afb41d39bc8b35f3fe46939b84c2
minhalee <minha1002@gmail.com>
2025-05-21 16:05:05 +0900

    feat: N+1 해결을 위해 batch size = 5 설정

--- src/main/java/org/eternity/domainmodel/movie/domain/DiscountPolicy.java
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountPolicy.java
@@ -3,6 +3,7 @@ package org.eternity.domainmodel.movie.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -15,6 +16,7 @@ public class DiscountPolicy {
     private Long id;
 
     @OneToMany(mappedBy = "policy", fetch = FetchType.LAZY)
+    @BatchSize(size = 5)
     private Set<DiscountCondition> conditions = new HashSet<>();
 
     public DiscountPolicy(DiscountCondition ... conditions) {
