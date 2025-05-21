4e53026c59eb4aced3622ce252ffa93a766ee718
darkelfand <darkelfand@gmail.com>
2025-05-21 16:13:38 +0900

    feat: 최순열 과제 제출

--- src/main/java/org/eternity/domainmodel/movie/domain/DiscountCondition.java
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountCondition.java
@@ -1,13 +1,20 @@
 package org.eternity.domainmodel.movie.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Entity
 @Getter
+@DiscriminatorColumn(name = "CONDITION_TYPE")
 public abstract class DiscountCondition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "condition_sequence")
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "POLICY_ID")
     private DiscountPolicy policy;
 
     abstract public void load();
