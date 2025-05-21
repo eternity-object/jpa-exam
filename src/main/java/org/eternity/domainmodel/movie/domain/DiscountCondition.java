5b90fc4bbb7eaeb3cb427a2a81072a86a31512be
DESKTOP-0UMLVO6\Kim <forteas2003@hanmail.net>
2025-05-21 21:37:11 +0900

    feat: 김윤제 과제 제출

--- src/main/java/org/eternity/domainmodel/movie/domain/DiscountCondition.java
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountCondition.java
@@ -1,13 +1,27 @@
 package org.eternity.domainmodel.movie.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@SequenceGenerator(
+        name = "condition_sequence",
+        sequenceName = "condition_sequence",
+        initialValue = 1, allocationSize = 50
+)
+@DiscriminatorColumn(name = "condition_type")
 public abstract class DiscountCondition {
+
+    @Id
+    @GeneratedValue(generator = "condition_sequence")
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "policy_id")
+    @Setter
     private DiscountPolicy policy;
 
     abstract public void load();
