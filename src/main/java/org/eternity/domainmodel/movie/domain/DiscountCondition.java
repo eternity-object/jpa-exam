f351898aa649ea710bf8aa45a0de7de950dcf6d3
kyungjin.kim <kyungjin.kim@ajway.kr>
2025-05-21 16:11:25 +0900

    실습문제-김경진

--- src/main/java/org/eternity/domainmodel/movie/domain/DiscountCondition.java
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountCondition.java
@@ -1,14 +1,30 @@
 package org.eternity.domainmodel.movie.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
 @Getter
+@SequenceGenerator(
+    name = "CONDITION_SEQ_GENERATOR",
+    sequenceName = "condition_sequence",
+    initialValue = 1,
+    allocationSize = 1
+)
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "CONDITION_TYPE")
 public abstract class DiscountCondition {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CONDITION_SEQ_GENERATOR")
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "policy_id")
     private DiscountPolicy policy;
+
+    public void setPolicy(DiscountPolicy policy) {
+        this.policy = policy;
+    }
 
     abstract public void load();
 }
--- src/main/java/org/eternity/domainmodel/movie/domain/DiscountPolicy.java
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountPolicy.java
@@ -1,6 +1,6 @@
 package org.eternity.domainmodel.movie.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,12 +10,22 @@ import java.util.Set;
 @Entity
 @NoArgsConstructor @Getter
 public class DiscountPolicy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToMany(mappedBy = "policy", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<DiscountCondition> conditions = new HashSet<>();
 
     public DiscountPolicy(DiscountCondition ... conditions) {
-        this.conditions = Set.of(conditions);
+        for (DiscountCondition condition : conditions) {
+            this.addCondition(condition);
+        }
+    }
+
+    private void addCondition(DiscountCondition condition) {
+        this.conditions.add(condition);
+        condition.setPolicy(this);
     }
 
     public void load() {
