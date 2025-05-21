c3f9f27e575fb280a5be57d060573d042d99c15b
inshining <inshining.kim@gmail.com>
2025-05-21 16:24:06 +0900

    solve: DiscountPolicy와 DiscountCondition에 Id를 추가

--- src/main/java/org/eternity/domainmodel/movie/domain/DiscountCondition.java
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountCondition.java
@@ -1,13 +1,22 @@
 package org.eternity.domainmodel.movie.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
+@SequenceGenerator(
+        name = "discount_condition_id_seq",
+        sequenceName = "condition_sequence",
+        initialValue = 1, allocationSize = 1
+)
 @Getter
 public abstract class DiscountCondition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "discount_condition_id_seq")
     private Long id;
 
+    @ManyToOne
     private DiscountPolicy policy;
 
     abstract public void load();
--- src/test/java/org/eternity/domainmodel/DiscountPolicyTest.java
+++ src/test/java/org/eternity/domainmodel/DiscountPolicyTest.java
@@ -1,8 +1,10 @@
 package org.eternity.domainmodel;
 
 import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.eternity.domainmodel.movie.domain.DiscountPolicy;
 import org.eternity.domainmodel.movie.domain.SequenceCondition;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -10,23 +12,42 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.util.List;
 import java.util.stream.IntStream;
 
+import static org.assertj.core.api.Assertions.*;
+
 @DataJpaTest(showSql = false)
 public class DiscountPolicyTest {
     @Autowired
     private EntityManager em;
 
+    @DisplayName("DiscountPolicy에 id가 IDENTIFY로 설정되어 있는지 확인한다.")
     @Test
-    public void lazy_load() {
-        IntStream.range(0, 4)
-                .forEach(index -> em.persist(new DiscountPolicy(new SequenceCondition(index), new SequenceCondition(index+1))));
+    void DiscountPolicy_id_is_IDENTIFY(){
+        DiscountPolicy discountPolicy = new DiscountPolicy(new SequenceCondition(1), new SequenceCondition(2));
+
+        em.persist(discountPolicy);
 
         em.flush();
         em.clear();
 
-        List<DiscountPolicy> policies = em.createQuery("select p from DiscountPolicy p").getResultList();
+        DiscountPolicy loaded = em.find(DiscountPolicy.class, discountPolicy.getId());
 
-        for(DiscountPolicy each : policies) {
-            each.load();
-        }
+        assertThat(loaded).isNotNull();
+        assertThat(loaded.getId()).isEqualTo(discountPolicy.getId());
+
     }
+
+//    @Test
+//    public void lazy_load() {
+//        IntStream.range(0, 4)
+//                .forEach(index -> em.persist(new DiscountPolicy(new SequenceCondition(index), new SequenceCondition(index+1))));
+//
+//        em.flush();
+//        em.clear();
+//
+//        List<DiscountPolicy> policies = em.createQuery("select p from DiscountPolicy p").getResultList();
+//
+//        for(DiscountPolicy each : policies) {
+//            each.load();
+//        }
+//    }
 }
