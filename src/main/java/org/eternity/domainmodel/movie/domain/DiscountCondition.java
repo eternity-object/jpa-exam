e19ff97e5dac82cb053fadb2c9e60fb5d63e0790
NSK <nswordan@nate.com>
2025-05-22 01:48:03 +0900

    1. DiscountPolicy와 DiscountCondition에 Id를 추가하세요.

--- src/main/java/org/eternity/domainmodel/movie/domain/DiscountCondition.java
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountCondition.java
@@ -1,11 +1,17 @@
 package org.eternity.domainmodel.movie.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
 @Getter
+@SequenceGenerator(
+        name = "condition_sequence",
+        sequenceName = "condition_sequence",
+        initialValue = 1, allocationSize = 50
+)
 public abstract class DiscountCondition {
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "condition_sequence")
     private Long id;
 
     private DiscountPolicy policy;
