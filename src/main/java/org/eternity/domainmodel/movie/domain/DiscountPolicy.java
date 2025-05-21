06d4bee4df4c7561fba5c1d865ac69103afca635
gitkidmk <lovekangmk@gmail.com>
2025-05-21 16:01:36 +0900

    feat - solve jpa test
    feat. gpt

--- src/main/java/org/eternity/domainmodel/movie/domain/DiscountPolicy.java
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountPolicy.java
@@ -1,6 +1,6 @@
 package org.eternity.domainmodel.movie.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,8 +10,13 @@ import java.util.Set;
 @Entity
 @NoArgsConstructor @Getter
 public class DiscountPolicy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToMany(mappedBy = "policy", cascade = CascadeType.ALL)
+    @org.hibernate.annotations.Fetch(
+            org.hibernate.annotations.FetchMode.SUBSELECT)
     private Set<DiscountCondition> conditions = new HashSet<>();
 
     public DiscountPolicy(DiscountCondition ... conditions) {
