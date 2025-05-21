aee99043737cf9e5c46cf9b57fda42d8be90b1c1
안상준 <twknds@hyundai-autoever.com>
2025-05-21 16:21:53 +0900

    안상준 jpa test

--- src/main/java/org/eternity/domainmodel/movie/domain/DiscountPolicy.java
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountPolicy.java
@@ -1,6 +1,12 @@
 package org.eternity.domainmodel.movie.domain;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,13 +14,18 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@NoArgsConstructor @Getter
+@NoArgsConstructor
+@Getter
 public class DiscountPolicy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToMany(mappedBy = "discountPolicy", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
     private Set<DiscountCondition> conditions = new HashSet<>();
 
-    public DiscountPolicy(DiscountCondition ... conditions) {
+    public DiscountPolicy(DiscountCondition... conditions) {
         this.conditions = Set.of(conditions);
     }
 
