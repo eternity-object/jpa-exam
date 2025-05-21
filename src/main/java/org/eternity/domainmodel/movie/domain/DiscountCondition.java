47f11a2b77824f067c6e58e11cb42a39e815d47c
yeoro <dufgh1009@gmail.com>
2025-05-21 16:03:00 +0900

    1. DiscountPolicy와 DiscountCondition에 Id를 추가하세요.
    - DiscountPolicy 클래스의 Id는 IDENTITY 유형으로 정의하고, DiscountCondition 상속 계층의 Id는 SEQUENCE 유형으로 정의합니다.
    - 이때 DB 시퀀스의 이름은 “condition_sequence”로 지으세요.

--- src/main/java/org/eternity/domainmodel/movie/domain/DiscountCondition.java
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountCondition.java
@@ -1,11 +1,20 @@
 package org.eternity.domainmodel.movie.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SequenceGenerator(
+        name = "condition_sequence"
+)
 public abstract class DiscountCondition {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "condition_sequence")
     private Long id;
 
     private DiscountPolicy policy;
