acb7b54d4ec94161411a4a614248fab962b6152c
NSK <nswordan@nate.com>
2025-05-22 16:21:57 +0900

    4. N+1 해결

--- src/main/java/org/eternity/domainmodel/movie/domain/DiscountPolicy.java
+++ src/main/java/org/eternity/domainmodel/movie/domain/DiscountPolicy.java
@@ -3,6 +3,7 @@ package org.eternity.domainmodel.movie.domain;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.BatchSize;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -13,6 +14,7 @@ public class DiscountPolicy {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToMany(mappedBy = "policy", fetch = FetchType.LAZY)
+    @BatchSize(size = 2)
     private Set<DiscountCondition> conditions = new HashSet<>();
 
     public DiscountPolicy(DiscountCondition ... conditions) {
